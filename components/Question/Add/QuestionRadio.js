84ea2941d833fea39245d535d6f88257be1aa7b4
mklx1319 <mklx1319@gmail.com>
2019-02-08 11:30:45 +0700

    change /lib redux

--- components/Question/Add/QuestionRadio.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-
-const QuestionRadio = (props) => <div />
-
-QuestionRadio.propTypes = {}
-
-export default QuestionRadio
--- pages/question/index.js
+++
@@ -1,6 +0,0 @@
-import Question from '../../components/Question'
-import React from 'react'
-
-const QuestionIndex = () => <Question />
-
-export default QuestionIndex
